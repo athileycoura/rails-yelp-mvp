41d978a2251a372bc1c2c32480985f31379c6b95
Athiley Luciano <athileyluciano@gmail.com>
2020-11-26 10:28:12 +0100

    restaurant in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+puts 'Deleting all restaurants...'
+Restaurant.destroy_all
+
+category = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+puts 'Creating 10 fake restaurants...'
+
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Company.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    phone_number: Faker::PhoneNumber,
+    category: category.sample
+  )
+  restaurant.save!
+  puts "#{restaurant.name} got created"
+end
+puts 'Finished!'
